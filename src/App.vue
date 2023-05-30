d9347e85f96ef587885f91efd80fa1d262cb1edf
Pablo Carballeda <pcarballeda@gmail.com>
2023-05-30 19:42:00 +0200

    Use TickerBoard scaffold and cleanup project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <script setup>
-import Keyboard from './components/keyboard/Keyboard.vue'
-import Content from './components/content/Content.vue'
+import Grid from './components/Grid.vue'
+
 // import { Cog6ToothIcon } from '@heroicons/vue/24/solid'
+components: {
+  Grid
+}
 </script>
 
 <template>
-  <div class="device">
-    <Content />
-    <Keyboard />
+  <div>
+    <Grid />>
   </div>
 </template>
 
-<style>
-#app {
-  display: flex;
-  justify-content: center;
-  background-color: #1a1a1a;
-}
-
-.device {
-  width: 360px;
-  height: 800px;
-  position: relative;
-  overflow: hidden;
-  background-color: #1a1a1a;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
-import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import store from './store'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
 const app = createApp(App)
 
 app.config.debug = true
@@ -18,5 +10,4 @@ app.config.devtools = true
 
 app.use(createPinia())
 app.use(store)
-app.use(vuetify)
 app.mount('#app')
